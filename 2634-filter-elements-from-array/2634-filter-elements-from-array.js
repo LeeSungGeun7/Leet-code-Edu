3b9e9773a073e1baf64aceb0191558d0c86f7387
LeeSungGeun7 <121940314+LeeSungGeun7@users.noreply.github.com>
2024-07-12 13:46:58 +0900

    Time: 51 ms (70.57%), Space: 49 MB (37.18%) - LeetHub

---
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+
+  let _array = []
+  arr.forEach((item,i) => {
+    if(fn(item, i)){
+      _array.push(item);
+    }
+  })
+  return _array;
+};
+
